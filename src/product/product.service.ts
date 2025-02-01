9d9726ff2491eaeea31c26d569a29f7a406e6ec0
gwang-coding <jaegwang1238@gmail.com>
2025-02-02 03:35:09 +0900

    get product async

---
+++ src/product/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>,
+  ) {}
+
+  //모든 상품가져오기
+  async getAllProducts(): Promise<Product[]> {
+    return this.productRepository.find();
+  }
+
+  //특정 상품 가져오기
+  async getProductById(id: number): Promise<Product | null> {
+    return this.productRepository.findOne({ where: { id } });
+  }
+}
