c89d5b25b46b0738d6ee681befb34d20f62770e1
gwang-coding <jaegwang1238@gmail.com>
2025-02-03 01:46:29 +0900

    add Cors

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.js 도메인만 허용
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
