14a32d6f5f893a3948d11547ea307051151adc65
gwang-coding <jaegwang1238@gmail.com>
2025-02-05 06:16:38 +0900

    feat: delete module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { Product } from './product/entities/product.entity';
