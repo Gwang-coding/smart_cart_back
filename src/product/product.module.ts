1a085cfdb19be6b4b1acc2cbbe5c949537fded67
gwang-coding <jaegwang1238@gmail.com>
2025-02-06 03:45:58 +0900

    feat: add websocket

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,10 +3,10 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
-
+import { WebSocketGatewayService } from 'src/gateway/websocket.gateway';
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, WebSocketGatewayService],
 })
 export class ProductModule {}
