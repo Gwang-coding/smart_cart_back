bf5a68f128d12b8754c306759c18d7558fb89e8f
gwang-coding <jaegwang1238@gmail.com>
2025-02-06 03:45:40 +0900

    improve: change from byId to handleBarcode

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,21 +1,23 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './entities/product.entity';
-
+import { WebSocketGatewayService } from 'src/gateway/websocket.gateway';
 @Controller('product')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+    private readonly websocketService: WebSocketGatewayService,
+  ) {}
 
-  //모든 유저 조회 API
-  @Get()
-  async getAllUsers(): Promise<Product[]> {
-    return this.productService.getAllProducts();
-  }
-
-  @Post(':barcode')
-  async getProductById(
-    @Param('barcode') barcode: string,
-  ): Promise<Product | null> {
-    return this.productService.getProductById(barcode);
+  @Post('scan')
+  async handleBarcode(
+    @Body('barcode') barcode: string,
+  ): Promise<Product | { message: string }> {
+    const product = await this.productService.getProductById(barcode);
+    if (!product) {
+      return { message: '상품을 찾을 수 없습니다.' };
+    }
+    this.websocketService.sendBarcodeData(product);
+    return product;
   }
 }
