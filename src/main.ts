3ccd0262ae097d4872c6f971b318719871b99532
gwang-coding <jaegwang1238@gmail.com>
2025-05-13 02:19:03 +0900

    Feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      whitelist: false,
+      forbidNonWhitelisted: false,
       transform: true,
     }),
   );
@@ -15,7 +16,12 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://192.168.0.7:3000',
-      'http://192.168.0.9',
+      'https://quickcheckout.netlify.app/',
+      'http://192.168.0.7',
+      'http://172.16.98.150:3000',
+      'http://172.30.1.82:3000',
+      'http://172.16.100.137:3000',
+      ' http://172.16.102.10:3000',
     ], // Next.js 도메인만 허용
   });
 
