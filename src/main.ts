3c405737bf7162fd9a6bfc3c24984c9e827c90f1
gwang-coding <jaegwang1238@gmail.com>
2025-05-14 17:56:47 +0900

    Feat: change port

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json());
@@ -12,19 +13,29 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const isDevelopment = process.env.NODE_ENV !== 'production';
+
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://192.168.0.7:3000',
-      'https://quickcheckout.netlify.app/',
-      'http://192.168.0.7',
-      'http://172.16.98.150:3000',
-      'http://172.30.1.82:3000',
-      'http://172.16.100.137:3000',
-      ' http://172.16.102.10:3000',
-    ], // Next.js 도메인만 허용
+    origin: isDevelopment
+      ? [
+          'http://localhost:3000',
+          'http://192.168.0.7:3000',
+          'http://192.168.0.7',
+          'http://172.16.98.150:3000',
+          'http://172.30.1.82:3000',
+          'http://172.16.100.137:3000',
+          'http://172.16.102.10:3000',
+        ]
+      : [
+          'https://quickcheckout.netlify.app',
+          // 프론트엔드 배포 URL 추가
+        ],
+    credentials: true, // 필요한 경우 쿠키/인증 관련 CORS 설정
   });
 
-  await app.listen(3001);
+  const port = process.env.PORT || 8080; // 기본 포트를 8080으로 변경
+  await app.listen(port);
+  console.log(`Application is running on port ${port}`);
 }
 bootstrap();
